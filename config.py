427b037121562574f56a20092933eef60d3ed4a6
AARABHSINGH <168511707+AARABHSINGH@users.noreply.github.com>
2024-05-25 13:48:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ API_ID = API_ID = 25434657
 
 API_HASH = os.environ.get("API_HASH", "22cfc54f94cf17360dc1475a51e38518")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6926015066:AAEWYAx_unRZdThWe5i9aDaJ2vfrZFFcQFg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6517893117:AAFC5OqAnTfI3P5syNFM0iejF225uIV0uZo")
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 6950434272))
 
-LOG = -1002209397514
+LOG = -1002013795533
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "6950434272").split()):
