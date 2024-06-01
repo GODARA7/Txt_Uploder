a2ca093a032b401e89fa9c2e40364d9821ac3011
Shankar <120117175+Saanusalvi1@users.noreply.github.com>
2024-06-02 00:01:16 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ API_ID = API_ID = 25434657
 
 API_HASH = os.environ.get("API_HASH", "22cfc54f94cf17360dc1475a51e38518")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7106246058:AAGcfd3ARNjiKwcRWqEnIowfJ_ERTOK1ywY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6998344235:AAGtWy227uTFi0aLr8N6W9fQvdq966_82WY")
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 6950434272))
 
-LOG = -1002205829205
+LOG = -1002209397514
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "6950434272").split()):
